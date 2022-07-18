e74d882a822bea8e1adbafbc5a4bb72711f10b0d
Rose-Zahra-Mohammadi <mohamadi.z.2016@gmail.com>
2022-07-18 10:15:14 -0400

    initial

---
+++ productManager/server/controllers/product.controller.js
@@ -0,0 +1,17 @@
+const {product, Product} = require('../models/product.model');
+module.exports.createProduct = (req, res) => {
+    Response.json({
+
+    })
+} 
+
+module.exports.createProduct = (req, res) => {
+    const { title, price, description } = req.body;
+    Product.create({
+        title,
+        price,
+        description
+    })
+        .then(product => res.json(product))
+        .catch(err => res.json(err))
+}
